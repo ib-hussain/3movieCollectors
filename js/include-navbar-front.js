864212f7e10a7fdc1b3d37a59f1b50e1467b9ccb
Ibrahim Hussain <ibrahimbeaconarion@gmail.com>
2025-12-04 21:52:32 +0500

    front navbar is good

--- js/include-navbar-front.js
+++ js/include-navbar-front.js
@@ -1,7 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
+    loadNavbar();
+});
+
+/* ===========================================
+   Load Navbar HTML
+=========================================== */
+function loadNavbar() {
     fetch("../components/navbar-front.html")
         .then(res => res.text())
         .then(html => {
-            document.body.insertAdjacentHTML("afterbegin", html);
-        });
-});
+            const container = document.getElementById("navbar-container");
+
+            if (container) {
+                container.innerHTML = html;
+            } else {
+                document.body.insertAdjacentHTML("afterbegin", html);
+            }
+
+            setActiveNavbarItem();
+        })
+        .catch(err => console.error("Navbar failed to load:", err));
+}
